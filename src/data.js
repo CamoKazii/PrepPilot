c1b434e5ed4370a2a0accf4a75e396725299c7a1
CamoKazii <cameronkbartels@gmail.com>
2026-08-08 22:19:38 +1000

    Add August Brisbane meal-prep recipe set
    Publish the selected breakfast, lunch and dinner with ingredient-verified macros and regression coverage.

--- src/data.js
+++ src/data.js
@@ -1,9 +1,10 @@
 import { breakfastRecipes } from './recipes/breakfast';
 import { lunchRecipes } from './recipes/lunch';
 import { dinnerRecipes } from './recipes/dinner';
+import { weeklyRecipes202608 } from './recipes/weekly-2026-08';
 
 export const targets = { calories: 2150, protein: 160, carbs: 210, fat: 70 };
 
-export const recipes = [...breakfastRecipes, ...lunchRecipes, ...dinnerRecipes];
+export const recipes = [...breakfastRecipes, ...lunchRecipes, ...dinnerRecipes, ...weeklyRecipes202608];
 
 export const bySlug = (slug) => recipes.find((recipe) => recipe.slug === slug);
